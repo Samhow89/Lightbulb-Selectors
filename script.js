9a2dc49112d69a55beafb2ce93677adb7b36898f
Sam Howard <essjay89@hotmail.co.uk>
2023-04-20 12:23:36 +0100

    Added JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Write your code here
+// Write your code here 
+
+let bulb1 = document.querySelector("#lightbulb1");
+let bulb2 = document.querySelector("#lightbulb2");
+let bulb3 = document.querySelector("#lightbulb3");
+
+let sub = document.querySelector(".subtitle");
+
+let count = 0;
+
+bulb1.addEventListener("click", function(){
+    count += 1;
+    sub.textContent = `You've clicked the light ${count} times`;
+    this.classList.toggle("active");
+});
+
+
+bulb2.addEventListener("click", function(){
+    count += 1;
+    sub.textContent = `You've clicked the light ${count} times`;
+    this.classList.toggle("active");
+});
+
+bulb3.addEventListener("click", function(){
+    count += 1;
+    sub.textContent = `You've clicked the light ${count} times`;
+    this.classList.toggle("active");
+});
